69bb3c60bb76599404b5f288ea3ba8ac10dd3e4c
Vieira <vieiranetoc@gmail.com>
2021-08-23 02:55:49 +0000

    remove js from jest
    Signed-off-by: Vieira <vieiranetoc@gmail.com>

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-};
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    testMatch: ['**/__tests__/**/*.ts?(x)', '**/?(*.)+(spec|test).ts?(x)']
+}
