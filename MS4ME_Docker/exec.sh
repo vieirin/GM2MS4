767130d4cab2745ae16a16df36fdf9bea4d941ac
mcoelhoneto <mcoelhoneto@pinterest.com>
2022-05-01 12:56:01 -0700

    MS4ME running on devcontainer

--- MS4ME_Docker/exec.sh
+++ MS4ME_Docker/exec.sh
@@ -1,15 +1,14 @@
 #!/usr/bin/env bash
 
+## These are commands for MacOS x11 forwarding it won't work for other
+## systems
+
+
 NAME=ms4me
 IP_ADDRESS=$(ifconfig en0 | grep "inet " | cut -d " " -f 2)
 DISPLAY="$IP_ADDRESS:0"
 
 startContainerOSX() {
-    APP=socat
-    brew ls --versions $APP > /dev/null 2>&1 || brew install $APP
-    APP=xquartz
-    brew ls --cask --versions $APP > /dev/null 2>&1 || brew install --cask $APP
-    
     sudo /opt/X11/bin/xhost + $IP_ADDRESS
     open -a XQuartz
     docker run -d \
@@ -45,4 +44,7 @@ buildContainer() {
     echo "\nContainer created, run ./start"
 }
 
-$1
+startX11MacOS() {
+    sudo /opt/X11/bin/xhost + $IP_ADDRESS
+    open -a XQuartz
+}
